7fac900b0a86f7c7b449b5f5a607a472e843db08
Kevin Porras <kporras07@gmail.com>
2016-06-25 13:35:38 -0600

    Try to install drupal in travis.

---
+++ travis/nyccampdemo.aliases.drushrc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * Wercker Drush Aliases.
+ */
+
+$aliases['nyccampdemo.dev'] = array(
+  'uri' => 'nyccampdemo.dev',
+  'root' => '/home/travis/build/kporras07/nyccampdemo/build',
+);
+
